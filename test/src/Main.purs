d8cf162fe8e599beb02c56206cd8152baa055d29
Sergei Dolgov <dolgovs@gmail.com>
2017-05-13 16:23:14 +0000

    test: Purescript 0.11

--- test/src/Main.purs
+++ test/src/Main.purs
@@ -1,22 +1,22 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, const, discard, pure, show, unit, ($), (<>), (>>>))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Aff (Aff, runAff)
-import Firebase
+import Control.Monad.Aff (runAff)
+import Firebase (FIREBASE)
 import Firebase.Authentication as FBA
 import Firebase.Database as FBD
-import Data.Argonaut
+import Data.Argonaut (encodeJson)
 
 main :: forall e. Eff (firebase :: FIREBASE, console :: CONSOLE | e) Unit
 main = do
   provider <- FBA.newGoogleProvider
-  runAff (show >>> log) (const $ pure unit) do
+  _ <- runAff (show >>> log) (const $ pure unit) do
     FBA.signInWithPopup provider
     liftEff do
-      FBD.onValue "/__test" $ \json -> do
+      _ <- FBD.onValue "/__test" $ \json -> do
         log $ "onValue: " <> show json
       log "Now writing..."
       FBD.write "/__test" $ encodeJson "99 bottles of beer on the wall, ..."
